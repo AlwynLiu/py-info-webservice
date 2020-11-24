1e516729d783dc1fc0621a4e8b85d07eb259f8fd
alvin.liu <1181285387@qq.com>
2020-11-24 23:24:57 +0800

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+counter = 100          # 整型变量
+miles = 1000.0       # 浮点型变量
+name = 'runoob'     # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+
+@app.route('/')
+def hello_world():
+    return str(10.2321 + 13213.1321)
+
+
+@app.route('/hello', methods=['post'])
+def hello():
+    return request.data
+
+# # 随便整点内容
+# 在整点内容
+# 变成多行的内容
+
+
+if __name__ == '__main__':
+    app.run()
