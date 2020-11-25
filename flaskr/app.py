d924ec749c541454630080be7a9cb49895d3b567
alvin.liu <1181285387@qq.com>
2020-11-25 22:56:35 +0800

    add template html file, but not work

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,26 +1,33 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 
 counter = 100          # 整型变量
 miles = 1000.0       # 浮点型变量
-name = 'runoob'     # 字符串
-
+name = range(100)  # 字符串
 # print(counter)
 # print(miles)
-# print(name)
+print(miles)
+
 
 
 @app.route('/')
 def hello_world():
-    return '测试0001'
+    return render_template('index.html')
 
 
-# @app.route('/hello', methods=['post'])
-# def hello():
-#     return request.data
+@app.route('/auth/login')
+def login():
+    # return render_template('auth/login.html')
+    return '登录页面'
+
+
+@app.route('/hello', methods=['get', 'post'])
+def hello():
+    return 'hello page'
 
 # # 随便整点内容
 # 在整点内容
