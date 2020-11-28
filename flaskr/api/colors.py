84b0c682552897c8dfb037a62d83a38148ebc3bd
alvin.liu <1181285387@qq.com>
2020-11-28 22:19:00 +0800

    添加flasgger的说明；

--- flaskr/api/colors.py
+++ flaskr/api/colors.py
@@ -12,6 +12,8 @@ def colors(palette):
     """获取颜色数据对象
     This is using docstrings for specifications.
     ---
+    tags:
+      - 获取数据
     parameters:
       - name: palette
         in: path
@@ -49,11 +51,13 @@ def colors(palette):
     return jsonify(all_colors)
 
 
-@app.route('/colors/login/', methods=['post'])
+@app.route('/user/login/', methods=['post'])
 def login():
-    """登录系统
+    """ 登入系统
     登录到系统获取颜色.
     ---
+    tags:
+      - 登入登出
     parameters:
       - name: palette
         in: path
@@ -80,6 +84,73 @@ def login():
           rgb: ['red', 'green', 'blue']
     """
     return '测试login api'
+
+
+@app.route('/user/loginout/', methods=['post'])
+def loginout():
+    """ 登出系统
+    登录到系统获取颜色.
+    ---
+    tags:
+      - 登入登出
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['全部', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: 颜色的列表(数据对象)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    return '测试loginout api'
+
+
+@app.route('/user/delete', methods=['delete'])
+def deleteUser():
+    """ 用户注销
+    用户注销账号.
+    ---
+    tags:
+      - 登入登出
+    parameters:
+      - name: userName
+        in: path
+        type: string
+        required: true
+      - name: password
+        in: path
+        type: string
+        required: true
+    definitions:
+      userName:
+        type: string
+      password:
+        type: string
+    responses:
+      200:
+        description: 删除了对象
+        schema:
+          $ref: '#/definitions/Palette'
+        examples: '注销失败'
+
+    """
+    return '测试loginout api'
 
 
 app.run(debug=True)
