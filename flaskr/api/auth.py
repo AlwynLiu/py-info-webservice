347d4714806f003bc2b71a2c7144c43866f05545
alvin.liu <1181285387@qq.com>
2020-12-07 22:05:25 +0800

    添加登录页面和接口；

--- flaskr/api/auth.py
+++ flaskr/api/auth.py
@@ -1,35 +1,35 @@
 import functools
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flasgger import Swagger
 
 # app = Flask(__name__)
 # swagger = Swagger(app)
 
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/delete', methods=['get'])
-def deleteUser():
-    """ 登出系统
-    登录到系统获取颜色.
+@bp.route('/login', methods=['get'])
+def loginSys():
+    """ 用户登录
+    用户登录到系统
     ---
     tags:
       - 登入登出
     parameters:
-      - name: palette
-        in: path
+      - userName: 
         type: string
-        enum: ['全部', 'rgb', 'cmyk']
+        required: true
+        default: all
+      - password: 
+        type: string
         required: true
         default: all
     responses:
       200:
-        description: 颜色的列表(数据对象)
+        description: success
         examples:
-          rgb: ['red', 'green', 'blue']
+          data: success
     """
-    return '测试loginout api'
-
-
+    return render_template('./auth/login.html')
