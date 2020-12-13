4fa148443adb36e31ba738077c7adc2c05033f30
alvin.liu 刘小维 <alvin.liu.os@infinitus-int.com>
2020-12-13 16:09:42 +0800

    处理文件上传和获取文件内容并打印；

--- flaskr/api/englishCount.py
+++ flaskr/api/englishCount.py
@@ -1,12 +1,31 @@
 from flask import Blueprint, render_template, request
-import string
+import string, json
 
 
 bp = Blueprint('englishcount', __name__, url_prefix='/englishcount')
 
 
-@bp.route('/wordCount', methods=['get', 'post'])
+@bp.route('/index', methods=['get'])
 def count():
+    """ 操作首页
+    返回可操作功能的首页
+    ---
+    tags:
+      - 单词计数
+    parameters:
+      - file:
+        type: string
+        default: all
+    responses:
+      200:
+        description: success
+        examples:
+          data: success
+    """
+    return render_template("./englishCount/index.html")
+
+@bp.route('/wordCount', methods=['post'])
+def postFile():
     """ 计算单词数量
     把上传的文件搞成计算成单词
     ---
@@ -22,53 +41,25 @@ def count():
         examples:
           data: success
     """
-    wordstring = """
-    /**
-        * Create a cached version of a pure function.
-        */
-          function cached(fn) {
-            var cache = Object.create(null);
-            return (function cachedFn(str) {
-              var hit = cache[str];
-              return hit || (cache[str] = fn(str))
-            })
-          }
-        
-          /**
-           * Camelize a hyphen-delimited string.
-           */
-          var camelizeRE = /-(\w)/g;
-          var camelize = cached(function (str) {
-            return str.replace(camelizeRE, function (_, c) { return c ? c.toUpperCase() : ''; })
-          });
-        
-          /**
-           * Capitalize a string.
-           */
-          var capitalize = cached(function (str) {
-            return str.charAt(0).toUpperCase() + str.slice(1)
-          });
-        
-          /**
-           * Hyphenate a camelCase string.
-           */
-    """
-
     # 读取页面上传的文件内容
-    postdata = request.files.get('uploadFile')
+    wordstring = request.files.get('uploadFile').read()
     # with open(request.files.get('uploadFile')) as file_obj:
     #     content = file_obj.read()
     #     print(content)
-    print(postdata)
     # 读取文本内容在单词频数
-    # wordstring = wordstring.replace('.', '')
-    # wordlist = wordstring.split()
-    # wordfreq = []
-    # for w in wordlist:
-    #     wordfreq.append(wordlist.count(w))
-    # d = dict(zip(wordlist, wordfreq))
-    # print(d)
+    wordstring = bytes.decode(wordstring)
+    wordstring = replaceStr(wordstring)
+    wordlist = wordstring.split()
+    wordfreq = []
+    for w in wordlist:
+        if(len(w) > 2):
+            wordfreq.append(wordlist.count(w))
+        else:
+            wordlist.remove(w)
+    d = dict(zip(wordlist, wordfreq))
+    return json.dumps(d, ensure_ascii=False)
 
-    return render_template("./englishCount/index.html")
-
-    # return "postdata"
+def replaceStr(str):
+    return str.replace('.', '').replace('{', '')\
+        .replace('}','').replace('(','')\
+        .replace(')','').replace('/*!','')
