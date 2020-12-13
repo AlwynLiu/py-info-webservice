5473c4f2699ab42a1e2831b68b73c674afc78c73
alvin.liu 小维 <alvin.liu.os@infinitus-int.com>
2020-12-13 20:40:45 +0800

    添加注释；

--- flaskr/api/englishCount.py
+++ flaskr/api/englishCount.py
@@ -43,33 +43,38 @@ def postFile():
     """
     # 读取页面上传的文件内容
     wordstring = request.files.get('uploadFile').read()
-    # with open(request.files.get('uploadFile')) as file_obj:
-    #     content = file_obj.read()
-    #     print(content)
+
     # 读取文本内容在单词频数
+    # 上文读取的数据是r''的字节数据，需要转化成string
     wordstring = bytes.decode(wordstring)
-    print(wordstring)
+
     wordstring = replaceStr(wordstring)
     wordlist = wordstring.split()
-    wordfreq = []
-    for w in wordlist:
-        if len(w) > 2:
-            wordfreq.append(wordlist.count(w))
-        else:
-            wordlist.remove(w)
-    d = dict(zip(wordlist, wordfreq))
+
     xmlString = ''
     for word in wordlist:
         if len(word) > 2 :
             xmlString += formatYoudaoXml(word, request.form['textText'])
     return '<wordbook>{}</wordbook>'.format(xmlString)
+
+    # 格式化处理为字段格式的json数据返回
+    # wordfreq = []
+    # for w in wordlist:
+    #     if len(w) > 2:
+    #         wordfreq.append(wordlist.count(w))
+    #     else:
+    #         wordlist.remove(w)
+    # d = dict(zip(wordlist, wordfreq))
     # return json.dumps(d, ensure_ascii=False)
     # return json.dumps(wordlist)
 
+
+# 替换不需要的字符
 def replaceStr(str):
     return str.replace('.', '').replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace('/*!', '').replace('《', '').replace('》', '')
 
 
+# 拼接成有道词典需要的xml格式item
 def formatYoudaoXml(word, tags='未分组'):
     return '''<item><word>{word}</word>
     <tags>{tags}</tags></item>'''.format(word=word, tags=tags)
