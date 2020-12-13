7ccfcf5d5061a5c013c633538b44f88aa55ca1fd
alvin.liu <1181285387@qq.com>
2020-12-13 15:03:15 +0800

    添加获取字符串单词频次；

--- flaskr/api/englishCount.py
+++ flaskr/api/englishCount.py
@@ -22,9 +22,53 @@ def count():
         examples:
           data: success
     """
-    postdata = request.form['textText']
-    with open(request.files.get('uploadFile')) as file_obj:
-        content = file_obj.read()
-        print(content)
+    wordstring = """
+    /**
+        * Create a cached version of a pure function.
+        */
+          function cached(fn) {
+            var cache = Object.create(null);
+            return (function cachedFn(str) {
+              var hit = cache[str];
+              return hit || (cache[str] = fn(str))
+            })
+          }
+        
+          /**
+           * Camelize a hyphen-delimited string.
+           */
+          var camelizeRE = /-(\w)/g;
+          var camelize = cached(function (str) {
+            return str.replace(camelizeRE, function (_, c) { return c ? c.toUpperCase() : ''; })
+          });
+        
+          /**
+           * Capitalize a string.
+           */
+          var capitalize = cached(function (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+          });
+        
+          /**
+           * Hyphenate a camelCase string.
+           */
+    """
+
+    # 读取页面上传的文件内容
+    postdata = request.files.get('uploadFile')
+    # with open(request.files.get('uploadFile')) as file_obj:
+    #     content = file_obj.read()
+    #     print(content)
     print(postdata)
+    # 读取文本内容在单词频数
+    # wordstring = wordstring.replace('.', '')
+    # wordlist = wordstring.split()
+    # wordfreq = []
+    # for w in wordlist:
+    #     wordfreq.append(wordlist.count(w))
+    # d = dict(zip(wordlist, wordfreq))
+    # print(d)
+
     return render_template("./englishCount/index.html")
+
+    # return "postdata"
