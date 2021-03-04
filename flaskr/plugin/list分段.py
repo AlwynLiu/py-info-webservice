de7fc4a6d59cdbac0bda624ebf71ad0348a6fbd3
刘小维 <alvin.liu.os@infinitus-int.com>
2021-03-04 16:30:51 +0800

    feat： 添加数组切分等量的子list；

---
+++ flaskr/plugin/list分段.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+__Author__ = "Alvin Liu"
+
+number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(0, len(number_list), 10):
+    print(number_list[i:i+10])
