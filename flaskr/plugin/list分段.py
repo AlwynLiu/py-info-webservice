b6a03289e3ae9e661e70a42c5ea680217d9e90eb
刘小维 <alvin.liu.os@infinitus-int.com>
2021-03-15 15:21:34 +0800

    feat: 实验切片获取数据；

--- flaskr/plugin/list分段.py
+++ flaskr/plugin/list分段.py
@@ -6,3 +6,7 @@ number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7,
 
 for i in range(0, len(number_list), 10):
     print(number_list[i:i+10])
+    
+    
+number_list1 = [3, 1, 2, 4]
+print(number_list1[1::])
